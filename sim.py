458f910021e01dcd3ae5238ec1162a96cf686442
rju <rju@purdue.edu>
2019-08-08 14:37:29 -0700

    Added verbose param

--- sim.py
+++ sim.py
@@ -6,16 +6,20 @@ from card import Deck
 import random
 
 MAX_SIMS = 100
+simtype = "Test"
+
+verbose = 0
+
 simnum = 0
 nwins = 0
 nloss = 0
-
 sum = 0
 results = []
 
 for simnum in range(MAX_SIMS):
 #    print(simnum)
-    print("")
+    if (verbose):
+        print("")
     mydeck = Deck()
     mydeck.shuffle()
     bjgame = BlackJack(1, mydeck)
@@ -27,9 +31,10 @@ for simnum in range(MAX_SIMS):
     playing = True
     while (playing):
 
-        print("Your current score is: " + str(myplayer.getscore()))
-        for mycard in myplayer.cards:
-            print(mycard)
+        if (verbose):
+            print("Your current score is: " + str(myplayer.getscore()))
+            for mycard in myplayer.cards:
+                print(mycard)
 
 # edge case: what if player holds an ace and a small card
 # ceiling will be higher than it should because we can reduce the entire score by 10    
@@ -47,9 +52,11 @@ for simnum in range(MAX_SIMS):
         else:
             probability = ((13 - ceiling + 1.0) * 4 - adjust) / decksize
 
-        print("Probability of losing: " + str(probability * 100) + "%")
+        if (verbose):
+            print("Probability of losing: " + str(probability * 100) + "%")
 
-        print("Select an option:")
+        if (verbose):
+            print("Select an option:")
 #        option = int(input("1. Stand \n2. Hit \n"))
         option = random.randint(1, 3)
 
@@ -58,27 +65,33 @@ for simnum in range(MAX_SIMS):
             playing = False
         elif (option == 2):
             bjgame.hit(myplayer)
-            print("Updated Score: " + str(myplayer.getscore()))
+            if (verbose):
+                print("Updated Score: " + str(myplayer.getscore()))
         else:
-            print("Please select a valid option!")
+            if (verbose):
+                print("Please select a valid option!")
     
         if (myplayer.getscore() > 21):
             playing = False
 
-    for mycard in myplayer.cards:
-        print(mycard)
+    if (verbose):
+        for mycard in myplayer.cards:
+            print(mycard)
 
     if (myplayer.getscore() > 21):
-        print("You lose")
+        if (verbose):
+            print("You lose")
         results.append(-1)
         nloss += 1
     else:
-        print("Final Score: " + str(myplayer.getscore()))
+        if (verbose):
+            print("Final Score: " + str(myplayer.getscore()))
         results.append(myplayer.getscore())
         nwins += 1
         sum += myplayer.getscore()
 
-    print("")
+    if (verbose):
+        print("")
 
 if (nwins + nloss != len(results)):
     print("")
@@ -88,7 +101,7 @@ if (nwins + nloss != len(results)):
 print("-----------------------------")
 print("Simulation Results")
 print("-----------------------------")
-#print(results)
+print("Simulation Type: " + simtype)
 print("Games Played: " + str(MAX_SIMS))
 lossratio = (1.0 * nloss / len(results))
 winrate = 1 - lossratio
@@ -101,4 +114,6 @@ print("")
 print("-----------------------------")
 print("End Simulation")
 print("-----------------------------")
+print("")
+
 
