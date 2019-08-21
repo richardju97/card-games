51b19265bbf2ac1b5ce8f6c314d5a85014e5b231
Cindy <cindyzhang977@gmail.com>
2019-08-20 22:18:40 -0700

    added basic test for black jack

--- data/results/test.py
+++ data/results/test.py
@@ -1,4 +1,37 @@
+# test.py
 # parse result files to ensure game results are correct and bots are correct
 
-def testBasicStrategyBot():
-    """parses all csv files with results for Basic Strategy Bot"""
+import os
+import csv
+
+def testBasic(playerScore, dealerScore, playerBusted, dealerBusted, gameResult):
+    """test the basics of blackjack game win/lose"""
+    testBust = lambda score: True of int(score) > 21 else False
+    assert playerBusted == testBust(playerEndHand), "playerBusted should be " + testBust(playerEndHand)
+    assert dealerBusted == testBust(dealerEndHand), "dealerBusted should be " + testBust(dealerEndHand)
+
+    testResult = lambda playerScore, dealerScore: "lose" if playerScore > 21 or (playerScore < dealerScore and dealerScore <= 21)
+                                                         else ("win" if playerScore > dealerScore else "tie")
+    assert gameResult == testResult(playerScore, dealerScore), "game result should be " + testResult(playerScore, dealerScore)
+
+def testBasicStrategyBot(file):
+    """parses csv file for Basic Strategy Bot and checks if output of game is valid"""
+    with open(file) as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            print("processing row: " + str(row))
+            if line_count != 0:
+                dealerFirstCard = row[0]
+                playerStartHand = row[1]
+                dealerEndHand = row[2]
+                playerEndHand = row[3]
+                playerBusted = row[4]
+                dealerBusted = row[5]
+                gameResult = row[6]
+                testBasic(playerEndHand, dealerEndHand, playerBusted, dealerBusted)
+            line_count += 1
+
+for file in os.listdir(os.getcwd()):
+    if file.split("_")[0] == "Basic":
+        testBasicStrategyBot(file)
