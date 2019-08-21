e424773507af30754c14b222c2f604d3634ab817
Cindy <cindyzhang977@gmail.com>
2019-08-21 07:29:39 -0700

    fixed buggy basic test

--- data/results/test.py
+++ data/results/test.py
@@ -4,13 +4,13 @@
 import os
 import csv
 
-def testBasic(playerScore, dealerScore, playerBusted, dealerBusted, gameResult):
+def testBasic(playerScore, dealerScore, playerBusted, dealerBusted, gameResult, line_count):
     """test the basics of blackjack game win/lose"""
-    testBust = lambda score: True of int(score) > 21 else False
-    assert playerBusted == testBust(playerEndHand), "playerBusted should be " + testBust(playerEndHand)
-    assert dealerBusted == testBust(dealerEndHand), "dealerBusted should be " + testBust(dealerEndHand)
+    testBust = lambda score: True if int(score) > 21 else False
+    assert playerBusted == testBust(playerScore), "playerBusted should be " + str(testBust(playerScore))
+    assert dealerBusted == testBust(dealerScore), "dealerBusted should be " + str(testBust(dealerScore))
 
-    testResult = lambda playerScore, dealerScore: "lose" if playerScore > 21 or (playerScore < dealerScore and dealerScore <= 21)
+    testResult = lambda playerScore, dealerScore: "lose" if playerScore > 21 or (playerScore < dealerScore and dealerScore <= 21) \
                                                          else ("win" if playerScore > dealerScore else "tie")
     assert gameResult == testResult(playerScore, dealerScore), "game result should be " + testResult(playerScore, dealerScore)
 
@@ -21,7 +21,7 @@ def testBasicStrategyBot(file):
         line_count = 0
         for row in csv_reader:
             print("processing row: " + str(row))
-            if line_count != 0:
+            if line_count != 0 and len(row) > 0:
                 dealerFirstCard = row[0]
                 playerStartHand = row[1]
                 dealerEndHand = row[2]
@@ -29,9 +29,10 @@ def testBasicStrategyBot(file):
                 playerBusted = row[4]
                 dealerBusted = row[5]
                 gameResult = row[6]
-                testBasic(playerEndHand, dealerEndHand, playerBusted, dealerBusted)
+                testBasic(playerEndHand, dealerEndHand, playerBusted, dealerBusted, gameResult, line_count)
             line_count += 1
 
 for file in os.listdir(os.getcwd()):
+    print(file)
     if file.split("_")[0] == "Basic":
         testBasicStrategyBot(file)
