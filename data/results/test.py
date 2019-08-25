e4f2082f3ecf1ee56093828f467ba0352fa1f1d3
Cindy <cindyzhang977@gmail.com>
2019-08-24 17:42:24 -0700

    working basic bj test

--- data/results/test.py
+++ data/results/test.py
@@ -4,20 +4,18 @@
 import os
 import csv
 
-def testBasic(playerScore, dealerScore, playerBusted, dealerBusted, gameResult, line_count):
+def testGameBasic(playerScore, dealerScore, playerBusted, dealerBusted, gameResult, line_count):
     """test the basics of blackjack game win/lose. playerScore/dealerScore/line_count are integers, while the rest are strings"""
-    testBust = lambda score: "True" if score > 21 else "False"
-    print("player score: " + str(playerScore), ", test bust result: " + str(testBust(playerScore)), ", player busted: " + str(playerBusted))
-    print("testBust == playerBusted ? " + str(testBust(playerScore) == playerBusted))
+    testBust = lambda score: True if score > 21 else False
     assert playerBusted == testBust(playerScore), "playerBusted should be " + str(testBust(playerScore)) + " in line " + str(line_count)
     assert dealerBusted == testBust(dealerScore), "dealerBusted should be " + str(testBust(dealerScore)) + " in line " + str(line_count)
-
-    testResult = lambda playerScore, dealerScore: "lose" if playerScore > 21 or (playerScore < dealerScore and dealerScore <= 21) \
-                                                         else ("win" if playerScore > dealerScore else "tie")
+    testResult = lambda playerScore, dealerScore: "lose" if playerBusted or (playerScore < dealerScore and not dealerBusted) \
+                                                         else ("win" if playerScore > dealerScore or dealerBusted else "tie")
     assert gameResult == testResult(playerScore, dealerScore), "game result should be " + testResult(playerScore, dealerScore) + " in line " + str(line_count)
 
 def testBasicStrategyBot(file):
     """parses csv file for Basic Strategy Bot and checks if output of game is valid"""
+    toBool = lambda str: True if str == "True" else False
     with open(file) as f:
         csv_reader = csv.reader(f, delimiter=',')
         line_count = 0
@@ -31,7 +29,7 @@ def testBasicStrategyBot(file):
                 dealerBusted = row[4]
                 playerBusted = row[5]
                 gameResult = row[6]
-                testBasic(int(playerEndHand), int(dealerEndHand), playerBusted, dealerBusted, gameResult, line_count)
+                testGameBasic(int(playerEndHand), int(dealerEndHand), toBool(playerBusted), toBool(dealerBusted), gameResult, line_count)
             line_count += 1
 
 for file in os.listdir(os.getcwd()):
