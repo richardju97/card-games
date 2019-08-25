b8228b8124f27f5df4649002853bf8b970afad10
Cindy <cindyzhang977@gmail.com>
2019-08-24 23:42:12 -0700

    static progress bar

--- web/src/simulation.js
+++ web/src/simulation.js
@@ -5,25 +5,44 @@ import './views/simulation.css';
 class Simulation extends React.Component {
   render() {
     return (
-      <div className='paragraph'>
-        <h2>Simulation</h2>
-        <p> Select a type of bot and the number of games to be simulated. </p>
-        <form className='simulation-form'>
-          Bot Type:
-          <select className='input'>
-            <option value='greedy'>Greedy</option>
-            <option value='probability'>Probability</option>
-            <option value='perceptron'>Perceptron</option>
-            <option value='basic strategy'>Basic Strategy</option>
-          </select>
-          <br />
+      <div>
+        <div className='paragraph'>
+          <h2>Simulation</h2>
+          <p> Select a type of bot and the number of games to be simulated. </p>
+          <form className='simulation-form'>
+            Bot Type:
+            <select className='input'>
+              <option value='greedy'>Greedy</option>
+              <option value='probability'>Probability</option>
+              <option value='perceptron'>Perceptron</option>
+              <option value='basic strategy'>Basic Strategy</option>
+            </select>
+            <br />
 
-          Number of simulations:
-          <input className='input' type='text' />
-          <br />
+            Number of simulations:
+            <input className='input' type='text' />
+            <br />
 
-          <button>Simulate</button>
-        </form>
+            <button>Simulate</button>
+          </form>
+        </div>
+        <ProgressBar />
+      </div>
+    );
+  }
+}
+
+class ProgressBar extends React.Component {
+  //tutorial: https://www.youtube.com/watch?v=DYevj6UGNWA
+  render() {
+    return (
+      <div className='progress-bar'>
+        <ul className='steps'>
+          <li className='visited'>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+        <div className='line'></div>
       </div>
     );
   }
