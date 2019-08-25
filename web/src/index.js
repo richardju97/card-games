ec3060ad6e2cf911713789abcb14494e51b1c640
Cindy <cindyzhang977@gmail.com>
2019-08-24 22:37:25 -0700

    bot descriptions

--- web/src/index.js
+++ web/src/index.js
@@ -5,7 +5,10 @@ import './views/index.css';
 class Home extends React.Component {
   render() {
     return (
-      <NavBar />
+      <div className='content'>
+        <NavBar />
+        <Instructions />
+      </div>
     );
   }
 }
@@ -14,12 +17,24 @@ function NavBar(props) {
   return (
     <div className='navbar'>
       <h1>Blackjack Bot</h1>
-      <div className='nav-links'>
-        <button><a href='https://github.com/richardju97/card-games' target="_blank" rel="noopener noreferrer">View Code</a></button>
-        <button>Authors</button>
-      </div>
+      <button><a href='https://github.com/richardju97/card-games' target='_blank' rel='noopener noreferrer'>View Code</a></button>
+      <button>Authors</button>
     </div>
   );
+}
+
+function Instructions(props) {
+  return (
+    <div className='paragraph'>
+      <h2>Bot Descriptions</h2>
+      <p>
+        <strong>Greedy: </strong>Always hits<br />
+        <strong>Probability: </strong>Based on probability threshold of winning<br />
+        <strong>Perceptron: </strong>Weighted decision making<br />
+        <strong>Basic Strategy: </strong>Based on strategy found <a className='link' href='https://bicyclecards.com/how-to-play/blackjack/?fbclid=IwAR21VaLpNnnYp5r5rDQkKJS-54RE7q-M8L9iMl0qawLGep0X4eR3bRdvC10' target='_blank' rel='noopener noreferrer'>here</a>
+      </p>
+    </div>
+  )
 }
 
 ReactDOM.render(
